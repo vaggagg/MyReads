54a6d7515b82f98181023d93168213c87e92723b
vaggelinho <vaggelinho10@gmail.com>
2020-09-30 04:13:58 +0300

    Some routing changing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom'
 import Category from './category';
+import Search from './search'
 import * as BooksAPI from './utils/BooksAPI';
 
 class App extends React.Component {
@@ -9,21 +11,30 @@ class App extends React.Component {
       c_reading:[],
       want_to_read:[],
       read:[]},
-      allBook:[]
+      allBooks:[]
       }
       componentDidMount() {
-        BooksAPI.getAll().then((books) => {
-          console.log(books)
+        BooksAPI.getAll().then((allBooks) => {
+          this.setState({allBooks:allBooks})
         })
-        console.log(this.state.shelves.c_reading)
+
       }
 render(){
   return (
     <div className="App">
+
       <div class="App-Title"> MyReads App</div>
+      <Route  path='/' render={() => (
+        <div>
       <Category name="Currently Reading" books={this.state.shelves.c_reading} />
       <Category name="Want to Read" books={this.state.shelves.want_to_read} />
       <Category name="Read" books={this.state.shelves.read} />
+      </div>
+                                    )} />
+    <Route path='/create' render={({ history }) => (
+      <Search />
+
+    )}/>
     </div>
   );
   }
---
+++ src/search.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+ class Search extends React.Component{
+  state={
+  }
+
+  render(){
+    return (
+      <div className="Search-Container">
+        Search panel
+      </div>
+            );
+          }
+}
+
+export default Search;
